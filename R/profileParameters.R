3b5dec63b44da5c2240f0fa94bea8d115b949e2f
jasenfinch <jsf9@aber.ac.uk>
2017-11-09 18:03:43 +0000

    added available methods to profileParameters

--- R/profileParameters.R
+++ R/profileParameters.R
@@ -6,72 +6,80 @@
 #' @importFrom erah setDecPar setAlPar
 #' @export
 
-profileParameters <- function(technique) {
-  parameters <- new('ProfileParameters',
-                    technique = technique,
-                    processingParameters = list()
-  )
+profileParameters <- function(technique = NULL) {
   
-  if (technique == 'GCMS-eRah') {
-    parameters@processingParameters <- list(
-      info = list(cls = 'class'),
-      deconvolution = setDecPar(min.peak.width = 1, avoid.processing.mz = c(54:69,73:75,147:149)),
-      alignment = setAlPar(min.spectra.cor = 0.90, max.time.dist = 1, mz.range = 70:600),
-      identification = list(DB = 'golm',
-                            path = 'GMD_20111121_VAR5_ALK_MSP.txt',
-                            DBname = 'GMD', 
-                            DBversion = 'GMD_20111121',
-                            DBinfo = "GOLM Metabolome Database
-                                      ------------------------
-                                      Kopka, J., Schauer, N., Krueger, S., Birkemeyer, C., Usadel, B., Bergmuller, E., Dor-
-                                      mann, P., Weckwerth, W., Gibon, Y., Stitt, M., Willmitzer, L., Fernie, A.R. and Stein-
-                                      hauser, D. (2005) GMD.CSB.DB: the Golm Metabolome Database, Bioinformatics, 21, 1635-
-                                      1638.",
-                            type = 'VAR5.ALK'
-                            )
+  availTechniques <- c('GCMS-eRah','GCMS-XCMS','LCMS-NP','LCMS-RP')
+  if (is.null(technique)) {
+    availTechniques <- paste(availTechniques,collapse = '\n\t\t\t')
+    availTechniques <- paste('\n\t\t\t',availTechniques,sep = '')
+    cat('\nAvailable Techniques:',availTechniques,sep = '')
+  } else {
+    parameters <- new('ProfileParameters',
+                      technique = technique,
+                      processingParameters = list()
     )
+    
+    if (technique == 'GCMS-eRah') {
+      parameters@processingParameters <- list(
+        info = list(cls = 'class'),
+        deconvolution = setDecPar(min.peak.width = 1, avoid.processing.mz = c(54:69,73:75,147:149)),
+        alignment = setAlPar(min.spectra.cor = 0.90, max.time.dist = 1, mz.range = 70:600),
+        identification = list(DB = 'golm',
+                              path = 'GMD_20111121_VAR5_ALK_MSP.txt',
+                              DBname = 'GMD', 
+                              DBversion = 'GMD_20111121',
+                              DBinfo = "GOLM Metabolome Database
+                              ------------------------
+                              Kopka, J., Schauer, N., Krueger, S., Birkemeyer, C., Usadel, B., Bergmuller, E., Dor-
+                              mann, P., Weckwerth, W., Gibon, Y., Stitt, M., Willmitzer, L., Fernie, A.R. and Stein-
+                              hauser, D. (2005) GMD.CSB.DB: the Golm Metabolome Database, Bioinformatics, 21, 1635-
+                              1638.",
+                              type = 'VAR5.ALK'
+        )
+      )
+    }
+    
+    if (technique == 'GCMS-XCMS') {
+      parameters@processingParameters <- list(
+        info = list(names = 'names', cls = 'class'),
+        peakDetection = MatchedFilterParam(
+          fwhm = 3, 
+          snthresh = 1
+        ),
+        grouping = PeakDensityParam(
+          bw = 5,
+          minFraction = 0
+        ),
+        retentionTimeCorrection = PeakGroupsParam(
+          smooth = 'loess'
+        ),
+        nCores = detectCores()
+      )
+    }
+    
+    if (technique == 'LCMS-RP' | technique == 'LCMS-NP') {
+      parameters@processingParameters <- list(
+        info = list(names = 'names', cls = 'class'),
+        peakDetection = CentWaveParam(
+          ppm = 1.5,
+          peakwidth = c(2,40),
+          snthresh = 3.0,
+          mzCenterFun = "apex", 
+          mzdiff = 0.05, 
+          fitgauss = FALSE,
+          integrate = 2
+        ), 
+        retentionTimeCorrection = ObiwarpParam(),
+        grouping = PeakDensityParam(
+          sampleGroups = 'class',
+          bw = 5,
+          binSize = 0.015, 
+          minFraction = 0
+        ),
+        nCores = detectCores()
+      )
+    }
+    
+    return(parameters)
   }
-  
-  if (technique == 'GCMS-XCMS') {
-    parameters@processingParameters <- list(
-      info = list(names = 'names', cls = 'class'),
-      peakDetection = MatchedFilterParam(
-        fwhm = 3, 
-        snthresh = 1
-      ),
-      grouping = PeakDensityParam(
-        bw = 5,
-        minFraction = 0
-      ),
-      retentionTimeCorrection = PeakGroupsParam(
-        smooth = 'loess'
-      ),
-      nCores = detectCores()
-    )
-  }
-  
-  if (technique == 'LCMS-RP' | technique == 'LCMS-NP') {
-    parameters@processingParameters <- list(
-      info = list(names = 'names', cls = 'class'),
-      peakDetection = CentWaveParam(
-        ppm = 1.5,
-        peakwidth = c(2,40),
-        snthresh = 3.0,
-        mzCenterFun = "apex", 
-        mzdiff = 0.05, 
-        fitgauss = FALSE,
-        integrate = 2
-      ), 
-      retentionTimeCorrection = ObiwarpParam(),
-      grouping = PeakDensityParam(
-        sampleGroups = 'class',
-        bw = 5,
-        binSize = 0.015, 
-        minFraction = 0
-      ),
-    nCores = detectCores()
-    )
-  }
-  
-  return(parameters)
 }
