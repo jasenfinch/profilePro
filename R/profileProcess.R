b935cc4b324939d034661857e5e19217db69750c
jasenfinch <jsf9@aber.ac.uk>
2019-02-04 15:27:55 +0000

    fixed profileProcess() example

--- R/profileProcess.R
+++ R/profileProcess.R
@@ -19,7 +19,7 @@
 #'               fileName = filesSplit[,ncol(filesSplit)], 
 #'               batch = rep(1,length(files)), 
 #'               batchBlock = rep(1,length(files)), 
-#'               names = str_replace_all(filesSplit[,ncol(filesSplit)],'.CDF',''), 
+#'               name = str_replace_all(filesSplit[,ncol(filesSplit)],'.CDF',''), 
 #'               class = filesSplit[,ncol(filesSplit)-1])
 #'
 #' files <- list(pos = files)
@@ -28,7 +28,7 @@
 #' parameters <- profileParameters('LCMS-RP')
 #' parameters@processingParameters$peakDetection <- CentWaveParam(snthresh = 20, noise = 1000)
 #' parameters@processingParameters$retentionTimeCorrection <- ObiwarpParam()
-#' parameters@processingParameters$grouping <- PeakDensityParam(maxFeatures = 300,minFraction = 2/3)
+#' parameters@processingParameters$grouping <- PeakDensityParam(sampleGroups = info$class,maxFeatures = 300,minFraction = 2/3)
 #' # run processing
 #' processedData <- profileProcess(files,info,parameters)
 #'
