d91f5703719f836ed09420cc76328d627198cc72
jasenfinch <jsf9@aber.ac.uk>
2019-02-05 16:03:03 +0000

    global variable definitions

--- R/profilePro.R
+++ R/profilePro.R
@@ -1,2 +1,4 @@
 
-globalVariables(c('rtmed','rtmin','rtmax','.','Mode','TIC','Median','Q1','Q3','Index','Colour','LowerOut','UpperOut'))
+globalVariables(c('rtmed','rtmin','rtmax','.','Mode','TIC','Median',
+                  'Q1','Q3','Index','Colour','LowerOut','UpperOut',
+                  'mzmin','Feature'))
