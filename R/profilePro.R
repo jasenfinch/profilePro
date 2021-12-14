216e4102bf09c5b28b2819baab564a9ed4576f5b
jasenfinch <jsf9@aber.ac.uk>
2021-12-14 13:23:11 +0000

    check fixes

--- R/profilePro.R
+++ R/profilePro.R
@@ -2,7 +2,8 @@
 globalVariables(c('rtmed','rtmin','rtmax','.','Mode','TIC','Median',
                   'Q1','Q3','Index','Colour','LowerOut','UpperOut',
                   'mzmin','feature','injOrder','AlignID','Name.1','Name',
-                  'Sample','Intensity','rtime','name','Class','intensity'))
+                  'Sample','Intensity','rtime','name','Class','intensity',
+                  'where'))
 
 #' @importFrom tidyselect vars_select_helpers
 tidyselect::vars_select_helpers$where
